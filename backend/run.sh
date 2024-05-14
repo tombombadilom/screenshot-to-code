5fa9b4b8d62010c39dca1f272501f15e6cc61d3c
TomBombadilom <tom@croll.fr>
2024-05-14 18:26:55 +0200

    updated run script

--- backend/run.sh
+++ backend/run.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/env bash
-source .env
+export POETRY_HOME=/opt/poetry
+
+# Create a new virtual environment using Python3 at the poetry home directory
+sudo python3 -m venv $POETRY_HOME
+
+# Upgrade pip in the virtual environment
+sudo $POETRY_HOME/bin/pip install --upgrade pip
+# Install the latest version of poetry
+echo "Installing the latest version of poetry..."
+sudo $POETRY_HOME/bin/pip install poetry==1.7
+sudo $POETRY_HOME/bin/pip install --upgrade poetry
+
+# Check the installed version of poetry
+sudo $POETRY_HOME/bin/poetry --version
+
+# Remove the existing poetry lock file if it exists
+echo "Removing the existing poetry lock file..."
+sudo rm poetry.lock
+
+# Generate a new lock file
+$POETRY_HOME/bin/poetry lock
+
+# Install the dependencies specified in the pyproject.toml file
+sudo $POETRY_HOME/bin/poetry install --no-root
+
+poetry install
+poetry shell
 poetry run uvicorn main:app --host 0.0.0.0 --port ${BACKEND_PORT:-7001}
